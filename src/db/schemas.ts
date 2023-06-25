c1a615ab5efeca637031af2692bf3c7ab29492f0
CarlosBarquin <116747942+CarlosBarquin@users.noreply.github.com>
2023-06-25 11:39:38 +0200

    Delete src directory

--- src/db/schemas.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectId } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-import { Book, Editorial } from "../types.ts";
-
-export type BookSchema = Omit<Book, "id" | "editorial"> & { _id: ObjectId, editorial : ObjectId };
-
-export type EditorialSchema = Omit<Editorial, "id" | "books"> & { _id: ObjectId };
